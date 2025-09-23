d15c357be0764bcf3538e0cc0b4f7ef515f5a8bd
“Shuraifp” <msharraf258@gmail.com>
2025-09-23 17:17:48 +0530

    fix(backend/src/main): removed trailing space programatically

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const FRONTEND_URL = process.env.FRONTEND_URL!.replace(/\/$/, "");
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
